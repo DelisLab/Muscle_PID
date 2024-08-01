d50873138f49fce56c7cb5e2d4b64c36093dd230
Davidoreilly12 <52181240+Davidoreilly12@users.noreply.github.com>
2024-08-01 12:14:38 +0100

    Update simulate_emg_data.m

--- Simulation/simulate_emg_data.m
+++ Simulation/simulate_emg_data.m
@@ -1,60 +1,56 @@
-function [EMG_1, EMG_2, task_param] = simulate_emg_data(N, noise_level, relationship_type, frequencies)
-    if nargin < 2
-        noise_level = 0.1; % Default noise level
+function [EMG_1, EMG_2, task_param] = simulate_emg_data(N,noise_correlation, frequencies,trials)
+ 
+   %Simulate two signals across multiple trials
+    synthetic_emg_1=[];synthetic_emg_2=[];
+    for i=1:trials
+        synthetic_emg_1 = [synthetic_emg_1;generate_synthetic_emg(N, frequencies)'];
+        synthetic_emg_2 = [synthetic_emg_2;generate_synthetic_emg(N, frequencies)'];
     end
-    if nargin < 3
-        error('You must specify the relationship type as: redundant, synergistic, or independent.');
-    end
-    if nargin < 4
-        frequencies = [30, 70, 150]; % Default frequencies
-    end
-
-    % Generate a binary task parameter
-    y = randi([0, 1], N, 1);
     
-   
-    synthetic_emg = normalize_emg(generate_synthetic_emg(N, frequencies)');
+    %Normalize the range of each signal
+    synthetic_emg_1=normalize_emg(synthetic_emg_1);
+    synthetic_emg_2=normalize_emg(synthetic_emg_2);
 
-    switch relationship_type
-        case 'redundant'
-            EMG_1 = (synthetic_emg.* y)+ (noise_level * randn(N, 1)); % Make two signals that encode the stimuli in a similar way
-            EMG_2 = (synthetic_emg .* y)+ (noise_level * randn(N, 1)); 
-            task_param = y; 
-        case 'synergistic'
-      
-            EMG_1 = (synthetic_emg.* y)+(noise_level * randn(N, 1)); 
-            EMG_2 = (synthetic_emg.* (1-y))+(noise_level * randn(N, 1)); % Make EMG_2 the functional complement of EMG_1
-            task_param = double(EMG_2>0.5 | EMG_1>0.5 & y==1); % Generate a conditional relationship between muscles and task
-          
-        case 'independent'
-            y2=randi([0, 1], N, 1);
-            EMG_1 = (synthetic_emg.* y) + (noise_level * randn(N, 1)); % Encode y in EMG_1
-            EMG_2 = (normalize_emg(generate_synthetic_emg(N, frequencies)'))+ (noise_level * randn(N, 1)) ;% Different EMG and task encoding
-            task_param = y; % Task parameter is y
-        otherwise
-            error('Unknown relationship type. Choose from: redundant, synergistic, or independent.');
+    % Step 2: Generate (anti-) correlated noise
+    mu = [0 0];  % Mean of the noise
+    sigma = [1 noise_correlation; noise_correlation 1];  % Covariance matrix
+    R = chol(sigma);  % Cholesky decomposition
+
+    % Generate the noise
+    noise1 = [];
+    noise2 = [];
+    for i = 1:trials
+        uncorrelatedNoise = randn(N, 2);  % Generate uncorrelated noise
+        correlatedNoise = uncorrelatedNoise * R;  % adjust correlational structure of the noise
+        noise1 = [noise1;correlatedNoise(:, 1)];
+        noise2 = [noise2;correlatedNoise(:, 2)];
     end
 
+    y=repmat([repelem(0,N)';repelem(1,N)'],trials);y=y(1:N*trials,1);% Generate a binary task parameter
+    EMG_1 = (synthetic_emg_1.* y)+ noise1; % Make two signals that encode this stimulus
+    EMG_2 = (synthetic_emg_2 .* y)+ noise2;
+    task_param = y; % Task parameter is y
 
-    % % Plot the results
-    figure;
 
-    subplot(1, 2, 1);
-    plot(EMG_1);
-    title([relationship_type, ': EMG 1']);
-
-    subplot(1, 2, 2);
-    plot(EMG_2);
-    title([relationship_type, ': EMG 2']);
-
-    % Output the simulated signals and task parameter
-    disp('Simulated EMG signals and task parameter:');
-    disp('EMG_1:');
-    disp(EMG_1);
-    disp('EMG_2:');
-    disp(EMG_2);
-    disp('Task parameter:');
-    disp(task_param);
+    % % % Plot the results
+    % figure;
+    % 
+    % subplot(1, 2, 1);
+    % plot(EMG_1);
+    % title([relationship_type, ': EMG 1']);
+    % 
+    % subplot(1, 2, 2);
+    % plot(EMG_2);
+    % title([relationship_type, ': EMG 2']);
+    % 
+    % % Output the simulated signals and task parameter
+    % disp('Simulated EMG signals and task parameter:');
+    % disp('EMG_1:');
+    % disp(EMG_1);
+    % disp('EMG_2:');
+    % disp(EMG_2);
+    % disp('Task parameter:');
+    % disp(task_param);
 end
 
 function synthetic_emg = generate_synthetic_emg(N, frequencies)
@@ -73,5 +69,5 @@ function normalized_emg = normalize_emg(emg_signal)
     % Normalize EMG signals to the range [0, 1]
     min_val = min(emg_signal);
     max_val = max(emg_signal);
-    normalized_emg = (emg_signal - min_val) / (max_val - min_val);
+    normalized_emg = (emg_signal - min_val) ./ (max_val - min_val);
 end
